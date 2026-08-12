af2a075d8a74c286351065ccb26bc7c12eee964f
jaromerobr <james201romero@gmail.com>
2026-08-12 18:35:41 -0500

    chore: proyecto base de la evaluacion preparado con flutter create y pub get

---
+++ lib/estudiante.dart
@@ -0,0 +1,6 @@
+// Datos de identificación de este examen.
+// No lo borres ni lo cambies: debe seguir apareciendo en la app
+// durante la revisión del profesor.
+const String estudianteNombre = 'James Luis Romero Bravo';
+const double miLatBase = -4.0067;
+const double miLngBase = -79.2101;
